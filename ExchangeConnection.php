d2205eb660b5bdd9624ab022d50b44937d5a989a
Lieske <Lieske@VIT001353.lt.lsa-net.de>
2018-06-05 14:08:35 +0200

    ExchangeConnection Exception hinzugefügt.

--- ExchangeConnection.php
+++ ExchangeConnection.php
@@ -91,23 +91,37 @@ class ExchangeConnection extends AbstractObservable implements Connection {
 
     /**
      * 
+     * @throws Exception
+     */
+    private function getImapConnection() {
+        $connection = imap_open($this->getConnectionAddress()
+            , $this->getUserName()
+            , $this->getPassword());
+        if (!$connection) {
+            throw new Exception('Connection not possible: check username/password');
+        } else {
+            $this->imapStream = $connection;
+        }
+    }
+
+    /**
+     * 
      */
     public function openConnection() {
         try {
-            $this->imapStream = imap_open($this->getConnectionAddress()
-                    , $this->getUserName()
-                    , $this->getPassword());
+            $this->getImapConnection();
             //über Observer in Logger schreiben 
             //Datum Uhrzeit Connection zum Postfach
             $this->setData('Connection successful ' .
-                    date("d.m.Y - H:i:s", time()));
+                date("d.m.Y - H:i:s", time()));
         } catch (Exception $exc) {
-            echo $exc->getTraceAsString();
+            //echo $exc->getTraceAsString();
             //über Observer in Logger schreiben
-            //Datum Uhrzeit Connection zum Postfach fehlgeschlagen
             $this->setData('Connection not successful' . ' ' .
-                    date("d.m.Y - H:i:s", time()) . ' ' .
-                    $exc->getTraceString());
+                date("d.m.Y - H:i:s", time()) . ' ' .
+                //$exc->getTraceAsString());
+                $exc->getMessage());
+            exit();
         }
     }
 
@@ -118,9 +132,8 @@ class ExchangeConnection extends AbstractObservable implements Connection {
         if (!is_null($this->imapStream)) {
             imap_close($this->getImapStream());
             //über Observer in Logger schreiben
-            //Connection zum Postfach geschlossen
             $this->setData('Connection closed' . ' ' .
-                    date("d.m.Y - H:i", time()));
+                date("d.m.Y - H:i:s", time()));
         }
     }
 
